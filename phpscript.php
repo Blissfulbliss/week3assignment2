bee94c59d91ef7cf837a39004ab37a22260e990c
Blissfulbliss <133139871+Blissfulbliss@users.noreply.github.com>
2023-05-25 10:10:14 +0100

    Create phpscript.php

---
+++ phpscript.php
@@ -0,0 +1,14 @@
+<?php
+
+$name = "Okah Blessing Chinecherem";
+$regnumber = "2021/HND/34119/CS";
+$soo = "Ebonyi State";
+$topic_of_project = "Personality Prediction System via CV Analysis";
+
+
+echo "Name: $name<br>";
+echo "Regnumber: $regnumber<br>";
+echo "State of origin: $soo<br>";
+echo "Project topic: $topic_of_project<br>";
+
+?>
